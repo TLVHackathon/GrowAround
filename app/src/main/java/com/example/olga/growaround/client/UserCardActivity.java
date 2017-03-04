1db16523164c0a25c916a18f25cc7421f3eb6850
shayl2 <beachb_sl@hotmail.com>
2017-03-04 23:36:54 +0200

    conflicts resolving

---
+++ app/src/main/java/com/example/olga/growaround/client/UserCardActivity.java
@@ -0,0 +1,19 @@
+package com.example.olga.growaround.client;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.olga.growaround.R;
+
+public class UserCardActivity extends AppCompatActivity {
+
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_card);
+
+
+    }
+
+}
---
+++ app/src/main/java/com/example/olga/growaround/viewcontroller/adapters/UserCardAdapter.java
@@ -0,0 +1,21 @@
+package com.example.olga.growaround.viewcontroller.adapters;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.view.LayoutInflater;
+import java.util.ArrayList;
+
+/**
+ * Created by Shay Lavi on 04/03/2017.
+ */
+
+public class UserCardAdapter {
+
+
+
+
+}
+
