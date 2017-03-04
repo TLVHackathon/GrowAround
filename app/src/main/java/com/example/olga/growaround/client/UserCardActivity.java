38878718c8b25e0651d2cbef40896eab7f074454
shayl2 <beachb_sl@hotmail.com>
2017-03-04 19:41:28 +0200

    User Card Screen - without BL

---
+++ app/src/main/java/com/example/olga/growaround/client/UserCardActivity.java
@@ -0,0 +1,15 @@
+package com.example.olga.growaround.client;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.olga.growaround.R;
+
+public class UserCardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_card);
+    }
+}
---
+++ app/src/main/java/com/example/olga/growaround/viewcontroller/adapters/UserCardAdapter.java
@@ -0,0 +1,8 @@
+package com.example.olga.growaround.viewcontroller.adapters;
+
+/**
+ * Created by Shay Lavi on 04/03/2017.
+ */
+
+public class UserCardAdapter {
+}
