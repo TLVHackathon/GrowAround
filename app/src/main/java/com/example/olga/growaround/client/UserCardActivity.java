ea59e907a27fca18020e7f7f78b5a56d70fe5be5
shayl2 <beachb_sl@hotmail.com>
2017-03-09 02:37:26 +0200

    User Card Screen - without BL. V2

--- app/src/main/java/com/example/olga/growaround/client/UserCardActivity.java
+++ app/src/main/java/com/example/olga/growaround/client/UserCardActivity.java
@@ -3,12 +3,24 @@ package com.example.olga.growaround.client;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.HorizontalScrollView;
+import android.widget.LinearLayout;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 
 import com.example.olga.growaround.R;
 import com.example.olga.growaround.manager.model.Card;
+import com.example.olga.growaround.viewcontroller.adapters.MainCardAdapter;
 
 public class UserCardActivity extends AppCompatActivity {
-    
+
+    ListView lstSearching;
+    MainCardAdapter mCardAdapter;
+
+    LinearLayout mLinearLayout;
+
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_card);
@@ -19,6 +31,16 @@ public class UserCardActivity extends AppCompatActivity {
             Card current = intent.getParcelableExtra("details");
         }
 
+        //replaceTextsOnScreen();
+
+/*
+        mCardAdapter = new MainCardAdapter(itemsList, UserCardActivity.this);
+        lstSearching = new ListView(UserCardActivity.this);
+        lstSearching.setAdapter(mCardAdapter);
+
+        mLinearLayout = (LinearLayout)findViewById(R.id.hsvSearching);
+        mLinearLayout.addView(lstSearching);
+*/
 
     }
 
