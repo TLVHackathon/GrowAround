4b8e06418cc695cf8e4a78799f9f307af58cbca5
shayl2 <beachb_sl@hotmail.com>
2017-03-10 02:58:32 +0200

    adding profile card

---
+++ app/src/main/java/com/example/olga/growaround/client/RequestActivity.java
@@ -0,0 +1,67 @@
+package com.example.olga.growaround.client;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.example.olga.growaround.R;
+import com.example.olga.growaround.manager.model.Card;
+import com.example.olga.growaround.viewcontroller.adapters.ItemsMapping;
+import com.example.olga.growaround.viewcontroller.views.ItemImageView;
+
+public class RequestActivity extends AppCompatActivity {
+
+    LinearLayout mLinearLayout;
+    ItemsMapping itemsMapping;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_request);
+
+
+        Intent intent = getIntent();
+
+        if(intent.hasExtra(getString(R.string.card_intent))) {
+            itemsMapping = new ItemsMapping();
+            Card current = intent.getParcelableExtra(getString(R.string.card_intent));
+
+            TextView username = (TextView)findViewById(R.id.txtUserName);
+            username.setText(current.getUserName());
+            TextView userDetails = (TextView)findViewById(R.id.txtUserDetails);
+            userDetails.setText(current.getUserDetails());
+
+
+            mLinearLayout = (LinearLayout)findViewById(R.id.lstSearching);
+            for (int itemIndex : current.getItemsSearch()) { //for (User user : userReceived) {
+                ItemImageView item = new ItemImageView(this);
+                //user.getVegetables().get(i);
+                item.setImageResource(itemsMapping.getItem(itemIndex));
+                mLinearLayout.addView(item);
+            }
+
+            mLinearLayout = (LinearLayout)findViewById(R.id.lstOffering);
+            for (int itemIndex : current.getItemsSearch()) { //for (User user : userReceived) {
+                ItemImageView item = new ItemImageView(this);
+                //user.getVegetables().get(i);
+                item.setImageResource(itemsMapping.getItem(itemIndex));
+                mLinearLayout.addView(item);
+            }
+
+            mLinearLayout = (LinearLayout)findViewById(R.id.lstGiving);
+            for (int itemIndex : current.getItemsSearch()) { //for (User user : userReceived) {
+                ItemImageView item = new ItemImageView(this);
+                //user.getVegetables().get(i);
+                item.setImageResource(itemsMapping.getItem(itemIndex));
+                mLinearLayout.addView(item);
+            }
+
+
+        }else{
+            // Error receiving information the user - go back with an error code.
+            finishActivity(-1);
+        }
+    }
+}
